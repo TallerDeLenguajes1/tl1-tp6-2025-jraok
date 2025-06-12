3b06a2d97105ec9b50b7f58ed7badb9296489ec7
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-12 19:33:28 -0300

    configuraciones en csproj y ejercicio1 terminado

--- TP06_Ejercicio1/Program.cs
+++ TP06_Ejercicio1/Program.cs
@@ -1,8 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+﻿int inverso = 0, numero = 0;
+bool bandera;
+string input;
+
+do
+{
+    Console.WriteLine("Ingrese un numero un numero entero (Enter para salir):");
+    input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Programa finalizado por el usuario.");
+        return;
+    }
+    bandera = int.TryParse(input, out numero);
+    if (!bandera || numero < 0)
+    {
+        Console.WriteLine("Entrada invalida reingrese");
+    }
+    else
+    {
+        int copia = numero;
+        while (copia > 0)
+        {
+            inverso = inverso * 10 + copia % 10;
+            copia /= 10;
+        }
+        Console.WriteLine($"El numero ingresado es: {numero} \nEl numero invertido es: {inverso}");
+    }
+    inverso = 0;
+} while (bandera || numero < 0);
+
