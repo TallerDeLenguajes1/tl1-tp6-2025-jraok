cc195f49faf776cb79f892a7f826cc3697203a5a
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-12 22:50:12 -0300

    correcciones minimas y agregado de comentarios, Ejercicio 2 terminado

--- TP06_Ejercicio2-3/Program.cs
+++ TP06_Ejercicio2-3/Program.cs
@@ -1,39 +1,46 @@
-﻿int numero1, numero2, opcion;
+﻿// variables para el programa
+int numero1, numero2, opcion;
 string input;
 bool bandera;
 do
 {
-    Console.WriteLine("\n-----Taller de lenguajes I------");
-    Console.WriteLine("\n-----Menu de opciones------");
-    Console.WriteLine("1-\tSumar");
-    Console.WriteLine("2-\tRestar");
-    Console.WriteLine("3-\tMultiplicar");
-    Console.WriteLine("4-\tDividir");
-    Console.WriteLine("5-\tSalir");
+    // menu de opciones
+    Console.WriteLine("\t\t\n-----Taller de lenguajes I------");
+    Console.WriteLine("\t\n-----Menu de opciones------");
+    Console.WriteLine("\t1__Sumar");
+    Console.WriteLine("\t2__Restar");
+    Console.WriteLine("\t3__Multiplicar");
+    Console.WriteLine("\t4__Dividir");
+    Console.WriteLine("\t5__Salir");
 
+    // lectura de la entrada
     input = Console.ReadLine();
-    bandera = int.TryParse(input, out opcion);
-    if (opcion < 1 || opcion > 5 || string.IsNullOrEmpty(input) || !bandera)
+    bandera = int.TryParse(input, out opcion); /* conversion de la entrada a int */
+    if (opcion < 1 || opcion > 5 || string.IsNullOrEmpty(input) || !bandera)    /* verificación de la opcion */
     {
         Console.WriteLine("Opcion invalida reingrese");
     }
-    else if (opcion != 5)
+    else if (opcion != 5)   /* si no seleccionamos salir entramos a la lógica del programa */
     {
+        // bucles para ingresar numeros validos
         do
         {
             Console.WriteLine("Ingrese el primer numero: ");
             bandera = int.TryParse(Console.ReadLine(), out numero1);
-            if (!bandera)Console.WriteLine("Numero invalido reingrese");              
+            if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
         } while (!bandera);
+
         do
         {
             Console.WriteLine("Ingrese el segundo numero: ");
             bandera = int.TryParse(Console.ReadLine(), out numero2);
-            if (!bandera)Console.WriteLine("Numero invalido reingrese");              
+            if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
         } while (!bandera);
 
+        /* switch de opciones */
         switch (opcion)
         {
+            /* resultados */
             case 1:
                 Console.WriteLine($"Resultado de la suma: {numero1 + numero2}");
                 break;
@@ -44,7 +51,7 @@ do
                 Console.WriteLine($"Resultado de la multiplicación: {numero1 * numero2}");
                 break;
             case 4:
-                if (numero2 != 0)
+                if (numero2 != 0) /* constrol de que no se divida por 0 */
                 {
                     Console.WriteLine($"Resultado de la division: {(float)numero1 / numero2}");
                 }
@@ -58,6 +65,6 @@ do
         }
     }else
     {
-        Console.WriteLine("Saliendo del programa");
+        Console.WriteLine("Saliendo del programa"); /* mensaje de salida */
     }
-} while (!bandera || opcion != 5);
+} while (opcion != 5);  /* control de la opcion del menu */
