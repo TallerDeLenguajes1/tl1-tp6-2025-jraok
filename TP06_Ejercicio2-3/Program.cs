2bdcb30e4753fe80bbac4e6366310aab95c4da03
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-15 00:09:16 -0300

    Version 2 completa

--- TP06_Ejercicio2-3/Program.cs
+++ TP06_Ejercicio2-3/Program.cs
@@ -1,75 +1,102 @@
 ﻿// variables para el programa
-int numero1, numero2, opcion;
+int opcion;
 string input;
 bool bandera;
 do
 {
     // menu de opciones
-    Console.WriteLine("\t\t\n-----Taller de lenguajes I------");
-    Console.WriteLine("\t\n-----Menu de opciones------");
-    Console.WriteLine("\t1__Sumar");
-    Console.WriteLine("\t2__Restar");
-    Console.WriteLine("\t3__Multiplicar");
-    Console.WriteLine("\t4__Dividir");
+    Console.WriteLine("\n\t\t-----Taller de lenguajes I------");
+    Console.WriteLine("\n\t-----Menu de opciones------");
+    Console.WriteLine("\t1-Valor Absoluto");
+    Console.WriteLine("\t2-Cuadrado");
+    Console.WriteLine("\t3-Raíz cuadrada");
+    Console.WriteLine("\t4-Seno");
+    Console.WriteLine("\t5-Coseno");
+    Console.WriteLine("\t6-Parte entera de un número decimal");
+    Console.WriteLine("\t7-Máximo entre dos números");
+    Console.WriteLine("\t8-Mínimo entre dos números");
+    Console.Write("\n\tIngrese una opción (Enter para cancelar): ");
 
     // lectura de la entrada
     input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input)) break;
     bandera = int.TryParse(input, out opcion); /* conversion de la entrada a int */
-    if (opcion < 1 || opcion > 5 || string.IsNullOrEmpty(input) || !bandera)    /* verificación de la opcion */
+    if (opcion < 1 || opcion > 9 || !bandera)    /* verificación de la opcion */
     {
         Console.WriteLine("Opcion invalida reingrese");
+        continue;
     }
-    else
+
+    if (opcion >= 1 && opcion <= 6)
     {
-        // bucles para ingresar numeros validos
+        float numero;
         do
         {
-            Console.WriteLine("Ingrese el primer numero: ");
-            bandera = int.TryParse(Console.ReadLine(), out numero1);
+            Console.Write("\nIngrese el numero a operar: ");
+            bandera = float.TryParse(Console.ReadLine(), out numero);
             if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
         } while (!bandera);
-
-        do
-        {
-            Console.WriteLine("Ingrese el segundo numero: ");
-            bandera = int.TryParse(Console.ReadLine(), out numero2);
-            if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
-        } while (!bandera);
-
-        /* switch de opciones */
         switch (opcion)
         {
-            /* resultados */
             case 1:
-                Console.WriteLine($"Resultado de la suma: {numero1 + numero2}");
+                Console.WriteLine($"Valor absoluto: {Math.Abs(numero)}");
                 break;
+
             case 2:
-                Console.WriteLine($"Resultado de la resta: {numero1 - numero2}");
+                Console.WriteLine($"Cuadrado: {numero * numero}");
                 break;
+
             case 3:
-                Console.WriteLine($"Resultado de la multiplicación: {numero1 * numero2}");
+                Console.WriteLine(numero >= 0 ? $"Raiz cuadrada {Math.Sqrt(numero)}" : "Error: no se puede sacar la raiz de un negativo");
                 break;
+
             case 4:
-                if (numero2 != 0) /* constrol de que no se divida por 0 */
-                {
-                    Console.WriteLine($"Resultado de la division: {(float)numero1 / numero2}");
-                }
-                else
-                {
-                    Console.WriteLine("Error no se puede dividir por cero");
-                }
+                Console.WriteLine($"Seno: {Math.Sin(numero)}");
                 break;
+
+            case 5:
+                Console.WriteLine($"Coseno: {Math.Cos(numero)}");
+                break;
+
             default:
+                Console.WriteLine($"Parte entera: {Math.Truncate(numero)}");
                 break;
         }
+
     }
-    // bucle para la opcion de la una nueva operacion
+    else if (opcion == 7 || opcion == 8)
+    {
+        float numero1, numero2;
+        do
+        {
+            Console.Write("Ingrese el primer numero: ");
+            bandera = float.TryParse(Console.ReadLine(), out numero1);
+            if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
+        } while (!bandera);
+
+        do
+        {
+            Console.Write("Ingrese el segundo numero: ");
+            bandera = float.TryParse(Console.ReadLine(), out numero2);
+            if (!bandera) Console.WriteLine("Valor invalido reingrese"); /* mensaje de error */
+        } while (!bandera);
+
+        if (opcion == 7)
+        {
+            Console.WriteLine($"Máximo: {Math.Max(numero1, numero2)}");
+        }
+        else
+        {
+            Console.WriteLine($"Mínimo: {Math.Min(numero1, numero2)}");
+        }
+    }
+    
     do
     {
-        Console.WriteLine("Desea realizar otra operacion?");
-        Console.WriteLine("1- Si \t 2- No");
+        Console.WriteLine("\nDesea realizar otra operacion?");
+        Console.WriteLine("\t\t1- Si \t 2- No");
         bandera = int.TryParse(Console.ReadLine(), out opcion);
-        if (opcion != 1 || opcion != 2 || !bandera) Console.WriteLine("Entrada invalida reingrese");
-    } while (opcion != 1 && opcion != 2 || !bandera);
+        if ((opcion != 1 && opcion != 2) || !bandera) Console.WriteLine("Entrada invalida reingrese");
+    } while ((opcion != 1 && opcion != 2) || !bandera);
     
-} while (opcion != 2);  /* control de la opcion del menu */
+} while (opcion == 1);  /* control de la opcion del menu */
